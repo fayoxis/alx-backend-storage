695d8e2fed874f7841c1807e1f6de885da05d2b1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-08 18:19:11 +0100

    Update 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,14 +1,11 @@
--- Drops the existing 'users' table if it exists
+-- Drops the 'users' table if it already exists
 DROP TABLE IF EXISTS users;
 
--- Creates a new table named 'users' with the following columns:
---   id (INT, NOT NULL, AUTO_INCREMENT, PRIMARY KEY)
---   email (VARCHAR(255), NOT NULL, UNIQUE)
---   name (VARCHAR(255), nullable)
---   country (CHAR(2), NOT NULL, DEFAULT 'US', CHECK constraint)
+-- Creates a table with unique users
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255),
-    country CHAR(2) NOT NULL DEFAULT 'US' CONSTRAINT country_check CHECK (country IN ('US', 'CO', 'TN'))
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Auto-incrementing primary key
+    email VARCHAR(255) NOT NULL UNIQUE, -- Unique email address
+    name VARCHAR(255), -- User's name
+    country CHAR(2) NOT NULL DEFAULT 'US' -- Country code (default is 'US')
+        CHECK (country IN ('US', 'CO', 'TN')) -- Allowed values: 'US', 'CO', 'TN'
 );
