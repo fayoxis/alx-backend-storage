6de3b8af9a32665e0abd471fdb7ef29dc27131b3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-08 18:26:56 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,8 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT
-    band_name,
-    COALESCE(
-        CASE
-            WHEN split IS NULL THEN YEAR(CURRENT_DATE()) - formed
-            ELSE split - formed
-        END,
-        0
-    ) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style LIKE '%Glam rock%'
-ORDER BY
-    lifespan DESC;
+-- script that lists all bands with Glam rock as their main style
+SELECT DISTINCT
+    `band_name`,
+    COALESCE(`split` - `formed`, 2024 - `formed`) AS `lifespan`
+FROM `metal_bands`
+WHERE `style` LIKE '%Glam rock%'
+ORDER BY `lifespan` DESC;
