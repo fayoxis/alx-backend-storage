ef88d5f7a71dd19340b4f605a761a0257d84046f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-08 19:01:05 +0100

    Update 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,35 +1,17 @@
--- Creates a stored procedure ComputeAverageWeightedScoreForUsers that
--- computes and store the average weighted score for all students.
+-- Creates a stored procedure
+-- that computes and store the average weighted score for all students
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    ALTER TABLE users ADD total_weighted_score INT NOT NULL;
-    ALTER TABLE users ADD total_weight INT NOT NULL;
-
-    UPDATE users
-        SET total_weighted_score = (
-            SELECT SUM(corrections.score * projects.weight)
-            FROM corrections
-                INNER JOIN projects
-                    ON corrections.project_id = projects.id
-            WHERE corrections.user_id = users.id
-            );
-
-    UPDATE users
-        SET total_weight = (
-            SELECT SUM(projects.weight)
-                FROM corrections
-                    INNER JOIN projects
-                        ON corrections.project_id = projects.id
-                WHERE corrections.user_id = users.id
-            );
-
-    UPDATE users
-        SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / users.total_weight);
-    ALTER TABLE users
-        DROP COLUMN total_weighted_score;
-    ALTER TABLE users
-        DROP COLUMN total_weight;
-END $$
+    UPDATE users U
+    JOIN (
+        SELECT U.id, SUM(C.score * P.weight) / SUM(P.weight) AS w_avg
+        FROM users U
+        JOIN corrections C ON U.id = C.user_id
+        JOIN projects P ON C.project_id = P.id
+        GROUP BY U.id
+    ) WA ON U.id = WA.id
+    SET U.average_score = WA.w_avg;
+END$$
 DELIMITER ;
