ec9d5db5f06994864cac1e1ef4a195cc3411e3cc
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-08 18:51:07 +0100

    Update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,15 +1,18 @@
--- creates a function SafeDiv that divides (and returns) the first
--- by the second number or returns 0 if the second number is equal to 0.
+-- This function, SafeDiv, performs division of two integers.
+-- If the divisor (second number) is non-zero, it returns the quotient.
+-- If the divisor is zero, it returns 0.
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS FLOAT DETERMINISTIC
+CREATE FUNCTION SafeDiv(dividend INT, divisor INT)
+RETURNS FLOAT
+DETERMINISTIC
 BEGIN
-    DECLARE result FLOAT DEFAULT 0;
+    DECLARE quotient FLOAT DEFAULT 0;
 
-    IF b != 0 THEN
-        SET result = a / b;
+    IF divisor <> 0 THEN
+        SET quotient = dividend / divisor;
     END IF;
-    RETURN result;
-END $$
+
+    RETURN quotient;
+END$$
 DELIMITER ;
