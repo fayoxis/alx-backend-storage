3344b2f588afb12c80462524b48b11846a8007bb
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-13 16:54:26 +0100

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-'''Task 8's module.
-'''
+"""
+Module for listing all documents in a MongoDB collection.
+"""
 
 
 def list_all(mongo_collection):
-    '''Lists all documents in a collection.
-    '''
+    """
+    Retrieves a list of all documents in the given MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB collection to list.
+
+    Returns:
+        list: A list of all documents in the collection.
+    """
     return [doc for doc in mongo_collection.find()]
