0aab4aa2c288801a2104c1b09e3841410c53d4fc
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-13 17:10:49 +0100

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
-'''Task 9's module.
-'''
+"""Module for Task 9"""
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''Inserts a new document in a collection.
-    '''
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    """
+    Python function that inserts a new document in a collection based on kwargs.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB collection to insert into.
+        **kwargs: The key-value pairs to be inserted as a document.
+
+    Returns:
+        str or None: The inserted document's ID if successful, otherwise None.
+    """
+    while mongo_collection is None:
+        return None
+
+    try:
+        result = mongo_collection.insert_one(kwargs)
+        return result.inserted_id
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return None
