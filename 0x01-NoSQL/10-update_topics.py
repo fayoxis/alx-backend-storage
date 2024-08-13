53e9c70ade3fbb92a3be26b0d3cf591519a589af
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-13 17:16:40 +0100

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
-'''Task 10's module.
-'''
+"""
+Update school topics
+"""
+from pymongo.collection import Collection
+from typing import List
 
 
-def update_topics(mongo_collection, name, topics):
-    '''Changes all topics of a collection's document based on the name.
-    '''
-    mongo_collection.update_many(
-        {'name': name},
-        {'$set': {'topics': topics}}
-    )
+def modify_subject_areas(collection: Collection, institution: str, subject_areas: List[str])
+-> pymongo.results.UpdateResult:
+    """
+    Update multiple documents with new subject areas.
+
+    Args:
+        collection (Collection): MongoDB collection to operate on.
+        institution (str): Name of the institution to update.
+        subject_areas (List[str]): New list of subject areas to set.
+
+    Returns:
+        pymongo.results.UpdateResult: Result of the update operation.
+    """
+    update_query = {"name": institution}
+    update_operation = {"$set": {"topics": subject_areas}}
+    
+    return collection.update_many(update_query, update_operation)
