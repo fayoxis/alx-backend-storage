c1a2125189a487227a3cb90776d9e4688888e3ba
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-13 18:09:01 +0100

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -29,5 +29,5 @@ def main():
         print(f"\tmethod {method}: {method_counts[method]}")
     print(f"{get_status} status check")
 
-while __name__ == "__main__":
+if __name__ == "__main__":
     main()
