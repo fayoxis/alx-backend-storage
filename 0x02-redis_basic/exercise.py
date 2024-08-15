c632b3588ac8001f141ac2c66c3cccc34287121a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-15 02:08:59 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -75,15 +75,7 @@ def call_history(method: Callable) -> Callable:
 
 
 def replay(fn: Callable) -> None:
-    """
-    Displays the call history of a Cache class' method.
-
-    This function retrieves the call count and the input and output values
-    for the specified method from the Redis data storage, and prints the
-    information to the console.
-
-    Args:
-        fn (Callable): The method to display the call history for.
+    """Displays Cache method that retrieves call count and i/o values
     """
     if fn is None or not hasattr(fn, '__self__'):
         return
@@ -163,28 +155,16 @@ class Cache:
                 transformed by the provided function.
         """
         data = self._redis.get(key)
-        return fn(data) if fn is not None else data
+       return fn(data) if fn is not None else data
 
     def get_str(self, key: str) -> str:
         """
         Retrieves a string value from a Redis data storage.
-
-        Args:
-            key (str): The key under which the data is stored.
-
-        Returns:
-            str: The retrieved data, decoded as a string.
         """
         return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
         """
         Retrieves an integer value from a Redis data storage.
-
-        Args:
-            key (str): The key under which the data is stored.
-
-        Returns:
-            int: The retrieved data, converted to an integer.
         """
         return self.get(key, lambda x: int(x))
