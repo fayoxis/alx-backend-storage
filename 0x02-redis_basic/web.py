2d0f61b43c5ce3e538d97896a805d73b499a6085
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-16 17:40:42 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,24 @@
+#!/usr/bin/env python3
+"""
+Defines a function `get_page`
+"""
+import redis
 import requests
-from functools import wraps
-from typing import Callable
-from cachetools import TTLCache
+from datetime import timedelta
 
-cache = TTLCache(maxsize=100, ttl=10)
 
-def cache_with_count(func: Callable) -> Callable:
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        count_key = f"count:{url}"
-        cached_result = cache.get(url)
-
-        if cached_result:
-            cache[count_key] = cache.get(count_key, 0) + 1
-            return cached_result
-
-        result = func(url)
-        cache[url] = result
-        cache[count_key] = cache.get(count_key, 0) + 1
-
-        return result
-
-    return wrapper
-
-@cache_with_count
 def get_page(url: str) -> str:
-    response = requests.get(url)
-    return response.text
-
-# Example usage
-url = "https://slowwly.robertomurray.co.uk/delay/3000/url/https://www.example.com"
-print(get_page(url))
-print(f"Count for {url}: {cache[f'count:{url}']}")
+    """
+    It uses the requests module to obtain
+    the HTML content of a particular URL and returns it.
+    Args:
+        url (str): url whose content is to be fectched
+    Returns:
+        html (str): the HTML content of the url
+    """
+    r = redis.Redis()
+    key = "count:{}{}{}".format('{', url, '}')
+    r.incr(key)
+    res = requests.get(url)
+    r.setex(url, timedelta(seconds=10), res.text)
+    return res.text
